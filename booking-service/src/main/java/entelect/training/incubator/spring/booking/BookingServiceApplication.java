b9814c3b037af6cb91a590e9016349c759814841
Damian de Freitas <damian.defreitas@entelect.co.za>
2023-03-14 10:52:31 +0200

    configured security for the booking service

--- booking-service/src/main/java/entelect/training/incubator/spring/booking/BookingServiceApplication.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/BookingServiceApplication.java
@@ -1,7 +1,12 @@
 package entelect.training.incubator.spring.booking;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BookingServiceApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(BookingServiceApplication.class, args);
     }
 }
