e493fcb44b2303755969db96ac12da8e304eb20d
Damian de Freitas <damian.defreitas@entelect.co.za>
2023-03-14 10:39:53 +0200

    setting up a model and repository for bookings

--- booking-service/src/main/java/entelect/training/incubator/spring/booking/controller/BookingsController.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/controller/BookingsController.java
@@ -1,4 +1,34 @@
 package entelect.training.incubator.spring.booking.controller;
 
+import entelect.training.incubator.spring.booking.service.BookingsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("customers")
 public class BookingsController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(BookingsController.class);
+
+    private final BookingsService bookingsService;
+
+    public BookingsController(BookingsService bookingsService) {
+        this.bookingsService = bookingsService;
+    }
+
+    /*@PostMapping
+    public ResponseEntity<?> createBooking(@RequestBody Customer customer) {
+        LOGGER.info("Processing customer creation request for customer={}", customer);
+
+        final Customer savedCustomer = customersService.createCustomer(customer);
+
+        LOGGER.trace("Customer created");
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }*/
+
 }
---
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/service/BookingsService.java
@@ -0,0 +1,19 @@
+package entelect.training.incubator.spring.booking.service;
+
+import entelect.training.incubator.spring.booking.model.Booking;
+import entelect.training.incubator.spring.booking.repository.BookingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingsService {
+
+    private final BookingRepository bookingRepository;
+
+    public BookingsService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public Booking createBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+}
